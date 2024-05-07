28cf4e37cda0a9bcc26cee92151b68a222132e9e
spacedawwwg <spacedawwwg@gmail.com>
2024-05-07 11:14:55 +0100

    chore: update dependencies

--- .eleventy.js
+++ .eleventy.js
@@ -1,22 +1,8 @@
 const fs = require('fs');
-const htmlmin = require('html-minifier');
 
 module.exports = (eleventyConfig) => {
   eleventyConfig.addLayoutAlias('base', 'base.njk');
   eleventyConfig.addPassthroughCopy({ _public: '/' });
-  eleventyConfig.addTransform('html-minifier', function (value, outputPath) {
-    if (outputPath.indexOf('.html') > -1) {
-      let minified = htmlmin.minify(value, {
-        useShortDoctype: true,
-        removeComments: true,
-        collapseWhitespace: true,
-        minifyCSS: true,
-        minifyJS: true,
-      });
-      return minified;
-    }
-    return value;
-  });
   eleventyConfig.setBrowserSyncConfig({
     callbacks: {
       ready: function (err, browserSync) {
